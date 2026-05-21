30e326245cd2aa27c12d046d40a2dd46d273ae7b
mechzolko-png <mechzolko@gmail.com>
2026-05-21 17:09:31 +0200

    valami

---
+++ Zoli/app.js
@@ -0,0 +1,8 @@
+async function loadPart(id, file) {
+  const response = await fetch(file);
+  const text = await response.text();
+  document.getElementById(id).innerHTML = text;
+}
+
+loadPart("header", "parts/header.html");
+loadPart("nav", "../Peti/nav.html");
